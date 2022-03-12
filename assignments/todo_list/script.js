79b5ade56fb75193c382e894cf46074203e702e8
grandheharshith <grandheharshith3@gmail.com>
2022-03-12 18:54:35 +0530

    todo list ass submission

---
+++ assignments/todo_list/script.js
@@ -0,0 +1,14 @@
+let count=0;
+let btn=document.querySelector("button");
+btn.addEventListener("click",adding);
+
+function adding(){
+    let name=document.querySelector("input").value;
+    let todos=document.querySelector(".todos");
+    let content=document.createElement("p");
+    content.setAttribute("key",count);
+    count+=1;
+    content.innerHTML=name;
+    todos.append(content);
+    content.addEventListener("click",content.remove);
+}
